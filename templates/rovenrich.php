249eeae296abfe02fa442f41cf7040b351bcd831
infivepvt <infivellc@gmail.com>
2025-05-06 10:17:03 +0530

    rovenrich8

--- templates/rovenrich.php
+++ templates/rovenrich.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="Images/Logo1.ico">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <title>Carl Protton - Business Card</title>
+    <title>Rovenrich - Business Card</title>
     <style>
         /* Basic reset and styling */
         * {
