a52d081493e725163919c052924e6cad366d0e8c
infivepvt <infivellc@gmail.com>
2025-05-16 17:28:46 +0530

    ghayani2

--- templates/ghayani.php
+++ templates/ghayani.php
@@ -277,11 +277,16 @@
             address: "No. 24, Wijerama Mawatha, Colombo 07",
             address2: "",
             about: "",
-            logo: "",
-            profileImage: "profile_img/client_profile/ghayani-p.png"
+            logo: ""
+            // Note: profileImage is removed since we'll handle it separately
         };
 
-        // Create VCF content
+        // Base64-encoded image (example placeholder)
+        // You need to convert your image (ghayani-p.png) to base64.
+        // You can use an online tool or a script to get the base64 string.
+        const profileImageBase64 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAA..."; // Replace with actual base64 string of ghayani-p.png
+
+        // Create VCF content with base64-encoded image
         let vcfContent = `BEGIN:VCARD
 VERSION:3.0
 FN:${contactData.firstName} ${contactData.lastName}
@@ -298,7 +303,7 @@ URL:${contactData.website3}
 ADR;TYPE=WORK:;;${contactData.address}
 ADR;TYPE=HOME:;;${contactData.address2}
 NOTE:${contactData.about}
-PHOTO;VALUE=URL:${contactData.profileImage}
+PHOTO;ENCODING=b;TYPE=PNG:${profileImageBase64.split(',')[1]} // Remove 'data:image/png;base64,' prefix
 LOGO;VALUE=URL:${contactData.logo}
 END:VCARD`;
 
