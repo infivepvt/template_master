972d11f46e3b0cdfe93638f9690333d0a32c7bc7
infivepvt <infivellc@gmail.com>
2025-05-19 13:22:36 +0530

    suduaraliay

--- templates/suduaraliya.php
+++ templates/suduaraliya.php
@@ -170,26 +170,32 @@
             <br>
             <div class="card-body text-center"
                 style="background-color: #FFFFFF; color: #000000; text-decoration: none; ">
-                <h2 class="card-title" style="font-size: 24px; text-transform: uppercase; margin-top:-20px">Hotel Sudu Araliya</h2>
+                <h2 class="card-title" style="font-size: 24px; text-transform: uppercase; margin-top:-20px">Hotel Sudu
+                    Araliya</h2>
                 <!-- <p style="color: #000000; font-size: 21px; text-transform: uppercase;"></p> -->
 
-                <div class="d-flex flex-wrap " style="padding-bottom:5px;margin-top:-10px"> 
-                    <a href="https://www.facebook.com/HotelSuduAraliya/" target="_blank" rel="noopener noreferrer" class="m-3">
+                <div class="d-flex flex-wrap " style="padding-bottom:5px;margin-top:-10px">
+                    <a href="https://www.facebook.com/HotelSuduAraliya/" target="_blank" rel="noopener noreferrer"
+                        class="m-3">
                         <img src="Images/Social_Media_Icon/facebook.png" alt="" style="width: 23px; height: 23px;">
                     </a>
-                    <a href="https://www.instagram.com/hotelsuduaraliya/" target="_blank" rel="noopener noreferrer" class="m-3">
+                    <a href="https://www.instagram.com/hotelsuduaraliya/" target="_blank" rel="noopener noreferrer"
+                        class="m-3">
                         <img src="Images/Social_Media_Icon/instagram.png" alt="" style="width: 23px; height: 23px;">
                     </a>
-                    <a href="https://www.tiktok.com/@hotelsuduaraliya" target="_blank" rel="noopener noreferrer" class="m-3">
+                    <a href="https://www.tiktok.com/@hotelsuduaraliya" target="_blank" rel="noopener noreferrer"
+                        class="m-3">
                         <img src="Images/Social_Media_Icon/tiktok.png" alt="" style="width: 23px; height: 23px;">
                     </a>
-                    <a href="https://www.youtube.com/@hotelsuduaraliya9930" target="_blank" rel="noopener noreferrer" class="m-3">
+                    <a href="https://www.youtube.com/@hotelsuduaraliya9930" target="_blank" rel="noopener noreferrer"
+                        class="m-3">
                         <img src="Images/Social_Media_Icon/youtube.png" alt="" style="width: 23px; height: 23px;">
-                    </a>                   
+                    </a>
                     <a href="https://x.com/araliyaclient" target="_blank" rel="noopener noreferrer" class="m-3">
                         <img src="Images/Social_Media_Icon/x-2.png" alt="" style="width: 23px; height: 23px;">
                     </a>
-                    <a href="https://www.linkedin.com/company/71032253" target="_blank" rel="noopener noreferrer" class="m-3">
+                    <a href="https://www.linkedin.com/company/71032253" target="_blank" rel="noopener noreferrer"
+                        class="m-3">
                         <img src="Images/Social_Media_Icon/linkedin.png" alt="" style="width: 23px; height: 23px;">
                     </a>
                 </div>
@@ -205,10 +211,10 @@
                         <img src="Images/Social_Media_Icon/linkedin.png" alt="" style="width: 45px; height: 45px;">
                     </a>
                 </div> -->
-            
 
-                <a href="https://api.whatsapp.com/send?phone=+94765315390&text=Hotel%20Sudu%20Araliya" target="_blank" rel="noopener noreferrer"
-                    style="text-decoration: none;">
+
+                <a href="https://api.whatsapp.com/send?phone=+94765315390&text=Hotel%20Sudu%20Araliya" target="_blank"
+                    rel="noopener noreferrer" style="text-decoration: none;">
                     <button class="btn w-100 mb-3 custom-phone1 d-flex align-items-center justify-content-center"
                         style="background-color: #25D366; color: white; height: 50px; border: 2px solid orange; border-radius: 10px; gap: 10px;">
                         <i class="fab fa-whatsapp" style="font-size: 30px; margin-right:30px;"></i>
@@ -223,7 +229,8 @@
                         onclick="window.open('tel:+94272224849', '_self')">
                         <img src="Images/icon/phone.png" alt="" style="width: 25px; height: 25px; margin-right:25px;">
                         <span class="text-center flex-grow-1 text-truncate custom-phone"
-                            style="color: #FFFFFF; font-family: 'Montserrat', sans-serif; font-size:16px">+94 272 224 849</span>
+                            style="color: #FFFFFF; font-family: 'Montserrat', sans-serif; font-size:16px">+94 272 224
+                            849</span>
                     </button>
 
                     <button type="button"
@@ -248,22 +255,28 @@
                         onclick="window.open('https://www.google.com/maps/place/Hotel+Sudu+Araliya/@7.9232387,80.9909973,17z/data=!4m9!3m8!1s0x3afb44ca2afcb6f3:0x5f0aa32de4b1b303!5m2!4m1!1i2!8m2!3d7.9232334!4d80.9935722!16s%2Fg%2F1vgw_56s?authuser=0&entry=ttu&g_ep=EgoyMDI1MDUxMy4xIKXMDSoASAFQAw%3D%3D', '_blank')">
                         <img src="Images/icon/map.png" alt="" style="width: 25px; height: 25px; margin-right:25px;">
                         <span class="text-center flex-grow-1 text-truncate custom-phone"
-                            style="color: #FFFFFF; font-family: 'Montserrat', sans-serif; font-size:15px;">Hotel Sudu Araliya, New town, <br>Polonnaruwa, Sri Lanka</span>
+                            style="color: #FFFFFF; font-family: 'Montserrat', sans-serif; font-size:15px;">Hotel Sudu
+                            Araliya, New town, <br>Polonnaruwa, Sri Lanka</span>
                     </button>
                 </div>
 
-                <p class="fw" style="font-family: 'Montserrat', sans-serif; font-size:15px; margin-top:-10px;">REVIEW US</p>
+                <p class="fw" style="font-family: 'Montserrat', sans-serif; font-size:15px; margin-top:-10px;">REVIEW US
+                </p>
                 <div style="margin-top:-20px;">
-                    <a href="https://g.page/r/CQOzseQtowpfEAI/review" target="_blank" rel="noopener noreferrer" class="m-3" style="text-decoration:none">
-                            <img src="Images/icon/google.png"  style="width: 45px; height: 45px;">
+                    <a href="https://g.page/r/CQOzseQtowpfEAI/review" target="_blank" rel="noopener noreferrer"
+                        class="m-3" style="text-decoration:none">
+                        <img src="Images/icon/google.png" style="width: 45px; height: 45px;">
                     </a>
-                    <a href="https://www.tripadvisor.com/UserReviewEdit-g304140-d577158-Hotel_Sudu_Araliya-Polonnaruwa_North_Central_Province.html" target="_blank" rel="noopener noreferrer" class="m-3" style="text-decoration:none">
-                            <img src="Images/icon/tripadvisor.png" alt="" style="width: 100px; height: 35px;margin-top:10px">
+                    <a href="https://www.tripadvisor.com/UserReviewEdit-g304140-d577158-Hotel_Sudu_Araliya-Polonnaruwa_North_Central_Province.html"
+                        target="_blank" rel="noopener noreferrer" class="m-3" style="text-decoration:none">
+                        <img src="Images/icon/tripadvisor.png" alt=""
+                            style="width: 100px; height: 35px;margin-top:10px">
                     </a>
-                    <a href="https://www.booking.com/hotel/lk/sudu-araliya.en-gb.html?label=gen173nr-1BCAsohQFCDHN1ZHUtYXJhbGl5YUgzWARohQGIAQGYAQm4ARjIAQzYAQHoAQGIAgGoAgS4AuufnMEGwAIB0gIkMDI5MmRmMDYtMTJjNC00YTZhLWE4NDktOTNlZWYwYmZlYTA52AIF4AIB&sid=55a24909d80faec8a736372abde41678&dist=0&group_adults=2&group_children=0&keep_landing=1&no_rooms=1&sb_price_type=total&type=total&" target="_blank" rel="noopener noreferrer" class="m-3" style="text-decoration:none">
+                    <a href="https://www.booking.com/hotel/lk/sudu-araliya.en-gb.html?label=gen173nr-1BCAsohQFCDHN1ZHUtYXJhbGl5YUgzWARohQGIAQGYAQm4ARjIAQzYAQHoAQGIAgGoAgS4AuufnMEGwAIB0gIkMDI5MmRmMDYtMTJjNC00YTZhLWE4NDktOTNlZWYwYmZlYTA52AIF4AIB&sid=55a24909d80faec8a736372abde41678&dist=0&group_adults=2&group_children=0&keep_landing=1&no_rooms=1&sb_price_type=total&type=total&"
+                        target="_blank" rel="noopener noreferrer" class="m-3" style="text-decoration:none">
                         <img src="Images/icon/booking.png" alt="" style="width: 40px; height: 40px;">
                     </a>
-                    
+
                 </div>
 
                 <!-- <div class="d-flex flex-wrap justify-content-center">
@@ -296,66 +309,61 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
     <script>
-    function generateVCF() {
-        // Updated contact information with proper image URL
-        const contactData = {
-            firstName: "Hotel Sudu Araliya",
-            lastName: "",
-            title: "",
-            phoneWork: "+94272224849",
-            phoneMobile: "",
-            email: "info@hotelsuduaraliya.com",
-            email2: "",
-            email3: "",
-            website: "https://www.hotelsuduaraliya.com", // Added https://
-            website2: "",
-            website3: "",
-            address: "Hotel Sudu Araliya,New town,Polonnaruwa,SriLanka",
-            address2: "",
-            about: "",
-            logo: "",
-            // Using full absolute URL for the profile image
-            profileImage: "https://tapilinq.com/profile_img/client_profile/suduaraliya-p.png"
-        };
+        function generateVCF() {
+            // Updated contact information with proper image URL
+            const contactData = {
+                firstName: "Hotel Sudu Araliya",
+                lastName: "",
+                title: "",
+                phoneWork: "+94272224849",
+                phoneMobile: "+94765315390",
+                email: "info@hotelsuduaraliya.com",
+                email2: "",
+                email3: "",
+                website: "http://www.hotelsuduaraliya.com",
+                website2: "",
+                website3: "",
+                address: "Hotel Sudu Araliya,New town,Polonnaruwa,SriLanka",
+                address2: "",
+                about: "Hotel Sudu Araliya in Polonnaruwa",
+                // Using base64 encoded image would be more reliable but makes the file larger
+                profileImage: "https://tapilinq.com/profile_img/client_profile/suduaraliya-p.png"
+            };
 
-        // Create properly formatted VCF content
-        let vcfContent = `BEGIN:VCARD
+            // Create properly formatted VCF content
+            let vcfContent = `BEGIN:VCARD
 VERSION:3.0
 FN:${contactData.firstName} ${contactData.lastName}
 N:${contactData.lastName};${contactData.firstName};;;
+ORG:Hotel Sudu Araliya;
 TITLE:${contactData.title}
 TEL;TYPE=WORK,VOICE:${contactData.phoneWork}
-TEL;TYPE=CELL:${contactData.phoneMobile}
-EMAIL:${contactData.email}
-EMAIL:${contactData.email2}
-EMAIL:${contactData.email3}
-URL:${contactData.website}
-URL:${contactData.website2}
-URL:${contactData.website3}
-ADR;TYPE=WORK:;;${contactData.address}
-ADR;TYPE=HOME:;;${contactData.address2}
+TEL;TYPE=CELL,VOICE:${contactData.phoneMobile}
+EMAIL;TYPE=WORK,INTERNET:${contactData.email}
+URL;TYPE=WORK:${contactData.website}
+ADR;TYPE=WORK,PREF:;;${contactData.address}
 NOTE:${contactData.about}
-PHOTO;TYPE=JPEG;ENCODING=b;VALUE=URI:${contactData.profileImage}
-LOGO;VALUE=URL:${contactData.logo}
+PHOTO;VALUE=URI;TYPE=JPEG:${contactData.profileImage}
+REV:${new Date().toISOString()}
 END:VCARD`;
 
-        // Create download link
-        const blob = new Blob([vcfContent], { type: 'text/vcard;charset=utf-8' });
-        const url = URL.createObjectURL(blob);
+            // Create download link
+            const blob = new Blob([vcfContent], { type: 'text/vcard;charset=utf-8' });
+            const url = URL.createObjectURL(blob);
 
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = `${contactData.firstName.replace(/\s+/g, '_')}.vcf`;
-        document.body.appendChild(a);
-        a.click();
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = `${contactData.firstName.replace(/\s+/g, '_')}.vcf`;
+            document.body.appendChild(a);
+            a.click();
 
-        // Clean up
-        setTimeout(() => {
-            document.body.removeChild(a);
-            URL.revokeObjectURL(url);
-        }, 100);
-    }
-</script>
+            // Clean up
+            setTimeout(() => {
+                document.body.removeChild(a);
+                URL.revokeObjectURL(url);
+            }, 100);
+        }
+    </script>
 </body>
 
 </html>
