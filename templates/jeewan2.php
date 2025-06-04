bdc98c28a79fc082309e84b0159b1bbc1e25f8ef
infivepvt <infivellc@gmail.com>
2025-06-04 09:48:58 +0530

    jwv6

--- templates/jeewan2.php
+++ templates/jeewan2.php
@@ -34,7 +34,7 @@
             padding: 120px 20px 70px;
             position: relative;
             text-align: center;
-             height: 250px;
+             height: 260px;
         }
 
         .profile-picture {
