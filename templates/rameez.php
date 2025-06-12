d78f5b4cbeee6ab15851a2ca646c04379dabc6a0
infivepvt <infivellc@gmail.com>
2025-06-12 13:58:26 +0530

    sdfs

--- templates/rameez.php
+++ templates/rameez.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Template 75</title>
+    <title>Live u</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="icon" type="image/x-icon" href="Images/Logo1.ico">
